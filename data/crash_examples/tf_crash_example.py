55492b6c651a3b205ad07640ec98c173deebcf5c
LiyanTao <641217688tly@gmail.com>
2024-08-06 17:36:21 +0800

    新增了Tensorflow的2.10.0所发布的CVEs(由checkmarx发布)

--- data/crash_examples/tf_crash_example.py
+++ data/crash_examples/tf_crash_example.py
@@ -14,3 +14,18 @@ result = tf.raw_ops.UnicodeDecodeWithOffsets(
     replace_control_characters=replace_control_characters,
     Tsplits=Tsplits,
 )
+"""
+CPU Env:
+To enable them in other operations, rebuild TensorFlow with the appropriate compiler flags.
+2024-08-06 17:21:52.452648: F tensorflow/core/framework/tensor.cc:725] Check failed: dtype() == expected_dtype (9 vs. 3) int32 expected, got int64
+/tmp/tmplxnwsmt6: line 3: 12851 Aborted                 python /mnt/e/1University/Research/DlibFuzz/Code/DlibFuzz/data/crash_examples/tf_crash_example.py
+ERROR conda.cli.main_run:execute(124): `conda run python /mnt/e/1University/Research/DlibFuzz/Code/DlibFuzz/data/crash_examples/tf_crash_example.py` failed. (See above for error)
+进程已结束，退出代码为 134
+
+GPU Env:
+2024-08-06 17:29:03.185225: I tensorflow/core/platform/cpu_feature_guard.cc:193] This TensorFlow binary is optimized with oneAPI Deep Neural Network Library (oneDNN) to use the following CPU instructions in performance-critical operations:  AVX2
+To enable them in other operations, rebuild TensorFlow with the appropriate compiler flags.
+2024-08-06 17:29:03.192289: I tensorflow/core/common_runtime/process_util.cc:146] Creating new thread pool with default inter op setting: 2. Tune using inter_op_parallelism_threads for best performance.
+2024-08-06 17:29:03.252938: F tensorflow/core/framework/tensor.cc:725] Check failed: dtype() == expected_dtype (9 vs. 3) int32 expected, got int64
+进程已结束，退出代码为 -1073740791 (0xC0000409)
+"""
--- data/crash_examples/tf_status_bug.py
+++ data/crash_examples/tf_status_bug.py
@@ -3,7 +3,36 @@ import tensorflow as tf
 data = ["a", "b", "c", "d", "e"]
 partitions = [3, -2, 2, -1, 2]
 num_partitions = 5
-t1 = tf.ragged.stack_dynamic_partitions(data, partitions, num_partitions)  # Succeed
-print(t1)
-# t2 = tf.ragged.stack(tf.dynamic_partition(data, partitions, num_partitions)) # Raise InvalidArgumentError
-# print(t2)
+# t1 = tf.ragged.stack_dynamic_partitions(data, partitions, num_partitions)  # Succeed
+# print(t1)
+"""
+2024-08-06 17:19:48.937092: W tensorflow/core/framework/op_kernel.cc:1780] OP_REQUIRES failed at bincount_op.cc:228 : INVALID_ARGUMENT: Input arr must be non-negative!
+Traceback (most recent call last):
+  File "/mnt/e/1University/Research/DlibFuzz/Code/DlibFuzz/data/crash_examples/tf_status_bug.py", line 6, in <module>
+    t1 = tf.ragged.stack_dynamic_partitions(data, partitions, num_partitions)  # Succeed
+  File "/home/tly/Applications/Programming/Python/Anaconda3/envs/DlibFuzz/lib/python3.9/site-packages/tensorflow/python/util/traceback_utils.py", line 153, in error_handler
+    raise e.with_traceback(filtered_tb) from None
+  File "/home/tly/Applications/Programming/Python/Anaconda3/envs/DlibFuzz/lib/python3.9/site-packages/tensorflow/python/framework/ops.py", line 7209, in raise_from_not_ok_status
+    raise core._status_to_exception(e) from None  # pylint: disable=protected-access
+tensorflow.python.framework.errors_impl.InvalidArgumentError: {{function_node __wrapped__Bincount_device_/job:localhost/replica:0/task:0/device:CPU:0}} Input arr must be non-negative! [Op:Bincount]
+ERROR conda.cli.main_run:execute(124): `conda run python /mnt/e/1University/Research/DlibFuzz/Code/DlibFuzz/data/crash_examples/tf_status_bug.py` failed. (See above for error)
+进程已结束，退出代码为 1
+"""
+
+t2 = tf.ragged.stack(tf.dynamic_partition(data, partitions, num_partitions)) # Raise InvalidArgumentError
+print(t2)
+"""
+Traceback (most recent call last):
+  File "/mnt/e/1University/Research/DlibFuzz/Code/DlibFuzz/data/crash_examples/tf_status_bug.py", line 22, in <module>
+    t2 = tf.ragged.stack(tf.dynamic_partition(data, partitions, num_partitions)) # Raise InvalidArgumentError
+  File "/home/tly/Applications/Programming/Python/Anaconda3/envs/DlibFuzz/lib/python3.9/site-packages/tensorflow/python/ops/gen_data_flow_ops.py", line 653, in dynamic_partition
+    return dynamic_partition_eager_fallback(
+  File "/home/tly/Applications/Programming/Python/Anaconda3/envs/DlibFuzz/lib/python3.9/site-packages/tensorflow/python/ops/gen_data_flow_ops.py", line 706, in dynamic_partition_eager_fallback
+    _result = _execute.execute(b"DynamicPartition", num_partitions,
+  File "/home/tly/Applications/Programming/Python/Anaconda3/envs/DlibFuzz/lib/python3.9/site-packages/tensorflow/python/eager/execute.py", line 54, in quick_execute
+    tensors = pywrap_tfe.TFE_Py_Execute(ctx._handle, device_name, op_name,
+tensorflow.python.framework.errors_impl.InvalidArgumentError: {{function_node __wrapped__DynamicPartition_num_partitions_5_device_/job:localhost/replica:0/task:0/device:CPU:0}} partitions[1] = -2 is not in [0, 5) [Op:DynamicPartition]
+ERROR conda.cli.main_run:execute(124): `conda run python /mnt/e/1University/Research/DlibFuzz/Code/DlibFuzz/data/crash_examples/tf_status_bug.py` failed. (See above for error)
+
+进程已结束，退出代码为 1
+"""
